6deec1cfdd3c66d8b1cde9f31f6c1e78705c2c10
Clayton Ritcher <claytonritcher@gmail.com>
2016-02-13 21:58:31 -0500

    FInished a 'rough' voice control node.

---
+++ src/voice_control/src/voice_control.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import rospy
+
+from std_msgs.msg import String
+from ez_kart_msgs.msg import Command
+
+commandMap = {"easy cart follow":Command.FOLLOW,
+              "easy cart stop":Command.STOP,
+              "easy cart shutdown":Command.SHUTDOWN}
+
+class voice_control:
+
+    def __init__(self):
+        self.msg = Command()
+        self.pub_ = rospy.Publisher('commands', Command)
+        rospy.Subscriber('recognizer/output', String, self.speechCallback)
+
+        r = rospy.Rate(10.0)
+        while not rospy.is_shutdown():
+            r.sleep()
+        
+    def speechCallback(self, msg):
+        for s in commandMap:
+            if s in msg.data:
+                self.msg.command = commandMap[s]
+                self.pub_.publish(self.msg)
+                break
+
+if __name__ == "__main__":
+    rospy.init_node('voice_control')
+    start = voice_control()
