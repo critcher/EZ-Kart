3477c9862c0d8be052e91fd897fc5a8dcdf4537c
ShastriRam <shastriram23@gmail.com>
2016-02-28 06:05:23 -0500

    Modified controller node to work using a class. Fixed all compilation errors. Finished up control loop.

--- src/controls/src/controller.cpp
+++ src/controls/src/controller.cpp
@@ -1,33 +1,141 @@
-#include "ros/ros.h"
-
 /* This node runs the control algorithm for basic skid steer 
    motion. The robot first corrects its angle error then 
    corrects the distance error. */
+#include <ros/ros.h>
+#include "ez_kart_msgs/motorControl.h"
+#include <math.h>
+#include <geometry_msgs/Pose2D.h>
 
-/* Define the PID gains. */ 
+/*!!!!!TO DO SUBSCRIBE TO STATUS MESSAGES !!!!!!!!*/
 
-static int kP = 0; 
-static int kI = 0; 
-static int kD = 0; 
+using namespace std; 
 
-void runPID()
+class Control
 {
+public: 
+  Control();
+private:
+  /*Variables of Control Class */ 
+  float kP; 
+  float kI; 
+  float kD;
+  float kartX; 
+  float kartY; 
+  float kartTheta; 
+  float angleError; 
+  float angleDeriv; 
+  float angleIntegral; 
+  float distError; 
+  float distDeriv; 
+  float distIntegral;
+  ros::NodeHandle n; 
+  ros::Timer timer; 
+  ros::Subscriber poseSub; 
+  ros::Publisher controlsPub;
+
+  /*Functions of the control class*/
+  //Control();
+  void runPID(const ros::TimerEvent&); 
+  void poseSubCallback(const geometry_msgs::Pose2D& msg); 
+};
+
+
+
+Control::Control()
+{
+  kP = 0; 
+  kI = 0; 
+  kD = 0; 
+  angleError = 0; 
+  angleDeriv = 0; 
+  angleIntegral = 0; 
+  distError = 0; 
+  distDeriv = 0; 
+  distIntegral = 0; 
+  kartX = 0; 
+  kartY = 0; 
+  kartTheta = 0; 
+
+  timer = n.createTimer(ros::Duration(0.1), &Control::runPID,this); 
+  poseSub = n.subscribe("name_pose_2d", 1000, &Control::poseSubCallback, this); 
+  controlsPub = n.advertise<ez_kart_msgs::motorControl>("/ezKartControl",1000); 
+}
+
+void Control::poseSubCallback(const geometry_msgs::Pose2D& msg)
+{
+  kartX = msg.x;
+  kartY = msg.y; 
+  kartTheta = msg.theta; 
+
+}
+
+void Control::runPID(const ros::TimerEvent&)
+{
+  /*Giving higher priority to angle error vs distance error.
+    I.e. correct for angle offset first, then distance. 
+    Assuming 0 degrees is straight forward*/
+
+  float dist = sqrt(pow(kartX,2) + pow(kartY,2)); 
+  distError = 0.6 - dist; 
+  distDeriv = distError/0.1; 
+  distIntegral = (distError * 0.1) + distIntegral;
   
+  angleError = 0 - kartTheta; 
+  angleDeriv = angleError/0.1; 
+  angleIntegral = (angleError * 0.1) + angleIntegral;
+
+  /*!!!!!!TO DO CHECK MOTOR DIRECTION WITH MOTOR CONTROLLER!!!!!!*/
+
+  ez_kart_msgs::motorControl ezKartControl;
+  if (abs(0 - angleError) > 0.01)
+    /*Angle offset.*/
+    {
+      if(angleError > 0) /*kart turned to left*/
+	{
+	  ezKartControl.leftMotorDir = 1; /*left forward*/
+	  ezKartControl.rightMotorDir = 0; /*right reverse*/
+	}
+      else
+	/*Kart turned to right*/
+	{
+	  ezKartControl.leftMotorDir = 0; /*left reverse*/
+	  ezKartControl.rightMotorDir = 1; /*right forward*/
+	}
+      float control = floor(abs((angleError * kP) + (angleIntegral * kI) + (angleDeriv * kD))); 
+      ezKartControl.leftMotorPWM = control; 
+      ezKartControl.rightMotorPWM = control; 
+      
+    }
+  else 
+    /*There is some distance offset but no angle offset*/
+    {
+      if (distError > 0.6) 
+	/*Kart further away from user than 2ft (0.6m) */
+	{
+	  ezKartControl.leftMotorDir = 0; /* left and right reverse*/
+	  ezKartControl.rightMotorDir = 0; 
+	}
+      else
+	/*Kart close to the user than 2ft (0.6m) */
+	{
+	  ezKartControl.leftMotorDir = 1; /* left and right forward*/
+	  ezKartControl.rightMotorDir = 1;
+	}
+      float control = floor(abs((distError * kP) + (distIntegral * kI) + (distDeriv * kD))); 
+      ezKartControl.leftMotorPWM = control; 
+      ezKartControl.rightMotorPWM = control;
+      
+    }
+  controlsPub.publish(ezKartControl); 
 }
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "controller"); 
-  ros::NodeHandle n; 
-
-  /*Setup timer */ 
-  ros::Timer timer1 = n.createTimer(ros::Duration(0.1), runPID);
-  
-  /*Setup subscriber to pose message from tracking. */
-
-  /*Setup publisher to publish control message */ 
-
-  ros::spin(); 
-
+  Control ezKartController;
+  while(ros::ok())
+  { 		    
+    ros::spinOnce();
+  }
   return 0; 
 }
