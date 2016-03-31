098fb99fdc0811c2881068d44dd8500475a071db
Mopewa Ogundipe <mogundip@andrew.cmu.edu>
2016-03-31 17:56:27 -0400

    Updated the sensors node to monitor status and light up the correct LEDs.

--- src/sensors/firmware/sensors.cpp
+++ src/sensors/firmware/sensors.cpp
@@ -1,5 +1,6 @@
 #include <ros.h>
 #include <ez_kart_msgs/Voltmeter.h>
+#include <ez_kart_msgs/Status.h>
 #include <sensor_msgs/Range.h>
 
 #include <Arduino.h>
@@ -7,10 +8,26 @@
 #define VOLTMETER_PIN A0
 #define ULTRASONIC_PIN A2
 
+#define LOST_PIN 2
+#define OBSTACLE_PIN 3
+#define BATTERY_PIN 4
+
 #define VOLTAGE_SCALE .07666
 #define DISTANCE_SCALE .005
 
 ros::NodeHandle nh;
+
+void updateLEDs(bool lost, bool obstacle, bool battery) {
+  digitalWrite(LOST_PIN, lost);
+  digitalWrite(OBSTACLE_PIN, obstacle);
+  digitalWrite(BATTERY_PIN, battery);
+}
+
+void statusCb(const ez_kart_msgs::Status& msg) {
+  updateLEDs(msg.lost, msg.obstacle,msg.low_voltage);
+}
+
+ros::Subscriber<ez_kart_msgs::Status> sub("status", &statusCb );
 
 ez_kart_msgs::Voltmeter voltage_msg;
 ros::Publisher voltage_pub("voltage", &voltage_msg);
@@ -28,7 +45,12 @@ float getDistance() {
 
 void setup()
 {
+  pinMode(LOST_PIN, OUTPUT);
+  pinMode(BATTERY_PIN, OUTPUT);
+  pinMode(OBSTACLE_PIN, OUTPUT);
+  updateLEDs(false, false, false);
   nh.initNode();
+  nh.subscribe(sub);
   nh.advertise(voltage_pub);
   nh.advertise(distance_pub);
 }
